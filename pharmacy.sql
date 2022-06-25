ff1d2d24c8b4d984664d147632cdf29de27e267b
emonShimoul <emonshimoul@gmail.com>
2022-06-25 23:37:34 +0600

    added new db

--- pharmacy.sql
+++ pharmacy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 24, 2022 at 09:43 PM
+-- Generation Time: Jun 25, 2022 at 07:36 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
@@ -59,10 +59,9 @@ CREATE TABLE `customers` (
 --
 
 INSERT INTO `customers` (`ID`, `NAME`, `CONTACT_NUMBER`, `ADDRESS`, `DOCTOR_NAME`, `DOCTOR_ADDRESS`) VALUES
-(13, 'Varsha Suthar', '7622369694', 'Rani Station', 'Dr Ramesh', 'Rani Station'),
 (14, 'Prakash Bhattarai', '9802851472', 'Pokhara-16, Dhikidada', 'Hari Bahadur', 'Matepani-12'),
-(15, 'Ghhgg', '1827282672', 'Khulna, Bangladesh', 'Ddsfdf', 'Khulna, Bangladesh'),
-(16, 'Aaa', '1234567890', 'Sonadanga, Khulna, Bangladesh', 'Dr  Strange', 'Dhaka, Bnagladesh');
+(16, 'Aaa', '1234567890', 'Sonadanga, Khulna, Bangladesh', 'Dr  Strange', 'Dhaka, Bnagladesh'),
+(18, 'Bbbb', '9898989898', '40, Ibrahim Mia Road, Shibbari', 'Dadasdesd', '40, Ibrahim Mia Road, Shibbari');
 
 -- --------------------------------------------------------
 
@@ -84,7 +83,8 @@ CREATE TABLE `invoices` (
 --
 
 INSERT INTO `invoices` (`INVOICE_ID`, `NET_TOTAL`, `INVOICE_DATE`, `CUSTOMER_ID`, `TOTAL_AMOUNT`, `TOTAL_DISCOUNT`) VALUES
-(1, 30, '2021-10-19', 14, 30, 0);
+(1, 30, '2021-10-19', 14, 30, 0),
+(5, 32.65, '2022-05-25', 13, 32.65, 0);
 
 -- --------------------------------------------------------
 
@@ -108,10 +108,9 @@ INSERT INTO `medicines` (`ID`, `NAME`, `PACKING`, `GENERIC_NAME`, `SUPPLIER_NAME
 (1, 'Nicip Plus', '10 TAB', 'Paracetamole', 'BDPL PHARMA'),
 (2, 'Crosin', '10 TAB', 'Hdsgvkvajkcbja', 'Kiran Pharma'),
 (4, 'Dolo 650', '15 TAB', 'Paracetamole', 'BDPL PHARMA'),
-(5, 'Gelusil', '10 TABLET', 'Mint Fla', 'Desai Pharma'),
 (7, 'Napa', '12TAB', 'Napa Plus', 'Napa Supplier'),
-(8, 'Napa', '10', 'Aaa', 'Gahgkakbvkv'),
-(9, 'Dolo 650', '5', 'Something', 'Kiran Pharma');
+(9, 'Dolo 650', '5', 'Something', 'Kiran Pharma'),
+(14, 'Asdasd', '10', 'Dasd', 'aaaa');
 
 -- --------------------------------------------------------
 
@@ -136,8 +135,8 @@ CREATE TABLE `medicines_stock` (
 INSERT INTO `medicines_stock` (`ID`, `NAME`, `BATCH_ID`, `EXPIRY_DATE`, `QUANTITY`, `MRP`, `RATE`) VALUES
 (1, 'Crosin', 'CROS12', '12/34', 2, 2626, 26),
 (2, 'Gelusil', 'G327', '12/42', 0, 15, 12),
-(3, 'Dolo 650', 'DOLO327', '01/23', 3, 30, 24),
-(4, 'Nicip Plus', 'NI325', '05/22', 1, 32.65, 28);
+(3, 'Dolo 650', 'DOLO327', '01/23', 1, 30, 24),
+(4, 'Nicip Plus', 'NI322', '05/22', 9, 32.65, 25);
 
 -- --------------------------------------------------------
 
@@ -159,8 +158,9 @@ CREATE TABLE `purchases` (
 --
 
 INSERT INTO `purchases` (`SUPPLIER_NAME`, `INVOICE_NUMBER`, `VOUCHER_NUMBER`, `PURCHASE_DATE`, `TOTAL_AMOUNT`, `PAYMENT_STATUS`) VALUES
-('Gahgkakbvkv', 1, 1, '2022-05-20', 30, 'PAID'),
-('Kiran Pharma', 2, 2, '2022-05-20', 100, 'DUE');
+('Kiran Pharma', 2, 2, '2022-05-21', 100, 'PAID'),
+('BDPL PHARMA', 1, 6, '2022-05-29', 20, 'DUE'),
+('Desai Pharmas', 5, 7, '2022-05-29', 3000, 'PAID');
 
 -- --------------------------------------------------------
 
@@ -184,19 +184,31 @@ INSERT INTO `suppliers` (`ID`, `NAME`, `EMAIL`, `CONTACT_NUMBER`, `ADDRESS`) VAL
 (1, 'Desai Pharmas', 'desai@gmail.com', '9948724242', 'Mahim East'),
 (2, 'BDPL PHARMA', 'bdpl@gmail.com', '8645632963', 'Santacruz West'),
 (9, 'Kiran Pharma', 'kiranpharma@gmail.com', '7638683637', 'Andheri East'),
-(10, 'Rsrnrnrndnn', 'ydj', '3737355538', '3fndfndfndndfnfdndfn'),
-(11, 'Dfnsfndfndf', 'fnsn', '5475734385', 'Ndnss4yrhrhdhrdhrh'),
-(12, 'SS Distributors', 'ssdis@gamil.com', '3867868752', 'Matunga West'),
-(13, 'Avceve', 'ehh', '3466626226', 'Eteh266266262'),
-(14, 'Hrshrhrjher', 'dzgdg', '4636347335', 'Rhrswjrnswjn'),
-(15, 'Hmrxfmgtmt', 'trmtrm gm tr', '6553838835', '38ejtdjtdxetjdt'),
-(20, 'Dtdxtkmtdshrrhhsrjrs', 'trmtrm gm tr', '6553838835', '38ejtdjtdxetjdt'),
-(23, 'Fndn', 'nena ena', '3462462642', 'Ebsbsdbsdndsnsdfns'),
-(24, 'Fndnbrwh', 'nena ena', '3462462642', 'Ebsbsdbsdndsnsdfns'),
-(25, 'Jnentjrtj', 'nena ena', '3462462642', 'Ebsbsdbsdndsnsdfns'),
-(26, 'Jerthjrtjtjr', 'nena ena', '3462462642', 'Ebsbsdbsdndsnsdfns'),
-(28, 'Gahgkakbvkv', 'nena ena', '3462462642', 'Ebsbsdbsdndsnsdfns'),
-(30, 'SupplierOne', 'sup1@mail.com', '1211211214', 'Dasdasdadad');
+(12, 'SS Distributors', 'ssdis@gamil.com', '3867868752', 'Matunga West');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `create_datetime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `create_datetime`) VALUES
+(3, 'bbb', 'bbb@mail.com', '003d81296cbed643579b24d1cf6c907c', '2022-06-12 20:16:29'),
+(4, 'aaa', 'aaa@mail.com', 'd9f6e636e369552839e7bb8057aeb8da', '2022-06-12 20:20:35'),
+(5, 'ccc', 'ccc@mail.com', '57a8bdf56042b790fee5c0d54f33a746', '2022-06-12 20:36:32'),
+(6, 'ddd', 'ddd@mail.com', '2523687e80f9e29ce70fbf25f546b31b', '2022-06-12 20:38:52');
 
 --
 -- Indexes for dumped tables
@@ -246,6 +258,12 @@ ALTER TABLE `suppliers`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -253,19 +271,19 @@ ALTER TABLE `suppliers`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `invoices`
 --
 ALTER TABLE `invoices`
-  MODIFY `INVOICE_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `INVOICE_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `medicines`
 --
 ALTER TABLE `medicines`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `medicines_stock`
@@ -277,13 +295,19 @@ ALTER TABLE `medicines_stock`
 -- AUTO_INCREMENT for table `purchases`
 --
 ALTER TABLE `purchases`
-  MODIFY `VOUCHER_NUMBER` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `VOUCHER_NUMBER` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `suppliers`
 --
 ALTER TABLE `suppliers`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
